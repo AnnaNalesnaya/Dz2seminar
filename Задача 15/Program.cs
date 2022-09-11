f2804d1fc7925a7fb59147fc6c373731d86b4257
Anna Nalesnaya <nalesnaya.anna@mail.ru>
2022-09-11 12:47:09 +0300

    Задача про дни недели

---
+++ Задача 15/Program.cs
@@ -0,0 +1,15 @@
+﻿//Пробую решить задачу дни недели
+Console.WriteLine("Введите номер дня недели ");
+int numDey = Convert.ToInt32(Console.ReadLine());
+if (numDey == 1 || (numDey == 2) || (numDey == 3) || (numDey == 4) || (numDey == 5))
+{
+    Console.WriteLine("рабочий");
+}
+else if (numDey == 6 || (numDey == 7))
+{
+    Console.WriteLine("выходной");
+}
+else
+{
+    Console.WriteLine("нет такого");
+}
